7d08202b803e08db1bb43758e74e1ae26be263c2
Roopak911 <roopaktelharkkar@yahoo.com>
2024-05-13 16:13:16 +0530

    updated provider.tf/bucketname, main.tf/key-pair

--- Jenkins-terraform/Main.tf
+++ Jenkins-terraform/Main.tf
@@ -58,10 +58,22 @@ resource "aws_security_group" "Jenkins-sg" {
   }
 }
 
+data "aws_ami" "ami" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
+  }
+
+  owners = ["099720109477"]
+}
+
+
 resource "aws_instance" "web" {
-  ami                    = "ami-0df4b2961410d4cff"
+  ami                    = data.aws_ami.ami.image_id
   instance_type          = "t2.medium"
-  key_name               = "purplehaze"
+  key_name               = "roopak -mumbai"
   vpc_security_group_ids = [aws_security_group.Jenkins-sg.id]
   user_data              = templatefile("./install_jenkins.sh", {})
   iam_instance_profile   = aws_iam_instance_profile.example_profile.name
--- Jenkins-terraform/provider.tf
+++ Jenkins-terraform/provider.tf
@@ -10,4 +10,5 @@ terraform {
 # Configure the AWS Provider
 provider "aws" {
   region = "ap-southeast-2"
+  shared_credentials_files = ["../../../../../.aws/credentials"]
 }
