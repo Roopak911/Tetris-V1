98ed4b1e1e43e2ef14f71cce943a51a67461fc60
Roopak911 <roopaktelharkkar@yahoo.com>
2024-05-13 16:21:54 +0530

    added dynamodb table to backend.tf

--- Jenkins-terraform/backend.tf
+++ Jenkins-terraform/backend.tf
@@ -3,5 +3,7 @@ terraform {
     bucket = "tetris-game-data-storage" # Replace with your actual S3 bucket name
     key    = "Jenkins/terraform.tfstate"
     region = "ap-south-1"
+    dynamodb_table = "Lock-Files"
+    encrypt        = true
   }
 }
