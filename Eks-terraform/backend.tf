626edc3922a3dbe917533bb381db47c4723f4a21
Roopak911 <roopaktelharkkar@yahoo.com>
2024-05-13 18:46:25 +0530

    backend.tf for eks

--- Eks-terraform/backend.tf
+++ Eks-terraform/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket         = "tetris-game-data-storage" # Replace with your actual S3 bucket name
-    key            = "EKS/terraform.tfstate"
+    key            = "Jenkins/terraform.tfstate"
     region         = "ap-south-1"
     dynamodb_table = "Lock-Files"
     encrypt        = true
